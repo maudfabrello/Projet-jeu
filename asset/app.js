054c47a1808eccb465f93e4a2dd8aa582da71123
maudfabrello <ma.fabrello@gmail.com>
2021-02-13 22:39:12 +0100

    finito du smaedi

--- asset/app.js
+++ asset/app.js
@@ -6,10 +6,12 @@ function plateauStarter(){
     for(let i=1;i<=10;i++)
     {
         rendu+="<div class=\"ligne "+i+"\">";
-        rendu+= "<div class=\"verif\"><div class=\"element1\"></div><div class=\"element2\"></div><div class=\"element3\"></div><div class=\"element4\"></div></div>";
-        rendu+= "<div class=\"jeux\"><div class=\"element1\"></div><div class=\"element2\"></div><div class=\"element3\"></div><div class=\"element4\"></div></div>";
+        rendu+= "<div class=\"verif flexeur\"><div class=\"element1\"></div><div class=\"element2\"></div><div class=\"element3\"></div><div class=\"element4\"></div></div>";
+        rendu+= "<div class=\"jeux flexeur\"><div class=\"element1\"></div><div class=\"element2\"></div><div class=\"element3\"></div><div class=\"element4\"></div></div>";
         rendu+= "</div>"
     }
     plateau.innerHTML= rendu;
 }
-plateauStarter();
+let startButton = document.getElementById('jouer');
+startButton.addEventListener('click',plateauStarter);
+// plateauStarter();
