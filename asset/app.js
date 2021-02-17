585d949fbe32da8f9a3acf71553d282f7c52d330
maudfabrello <ma.fabrello@gmail.com>
2021-02-17 11:00:49 +0100

    modified12

--- asset/app.js
+++ asset/app.js
@@ -19,7 +19,15 @@ function deleteColor()
     pawnList.forEach(item => {
         item.addEventListener('click',function()
         {
+            button(true);
+            let buttonVerifHide= document.getElementsByTagName('button') [0];
+            if(this.parentElement.parentElement.classList[1]==string)
+            {
             this.className=this.classList[0];
+            if (document.contains(buttonVerifHide)){
+                buttonVerifHide.remove();
+            }
+        }
         })
     })
 }
@@ -37,12 +45,14 @@ function shufflePawns()
 function verif(stringToverify)
 {
     var buttonVerif = document.createElement("button");
-    var textContent = document.createTextNode('Ok'); // effacerait la ligne si joueur n'est pas sûr de sa combinaison ?
+    var textContent = document.createTextNode('Ok'); // validerait la ligne quand joueur est sûr de sa combinaison
     buttonVerif.appendChild(textContent);
     stringToverify.appendChild(buttonVerif);
     buttonVerif.addEventListener('click',function(){
         button(true)
         buttonVerif.remove();
+        verifString(string);
+        string = string+1;
     })
 }
 function button(active){
@@ -67,8 +77,18 @@ function putColor(){
             {
                 item.classList.add(this.classList[1]);
                 item.classList.add('round');
-                if(item.classList[0]==='element4')
-                {
+                let showButton = false
+                let arrayNotClear = item.parentElement.children;
+                for(let i = 1; i <= arrayNotClear.length;i++){
+                    if(arrayNotClear[i-1].classList[1]!=undefined)
+                    {
+                        showButton=true
+                    }
+                    else{
+                        showButton=false
+                    }
+                }
+                if (showButton) {
                     verif(item.parentElement.parentElement);
                     button(false);
                 }
@@ -80,31 +100,93 @@ function putColor(){
     {
         if(e !== BreakException) throw e;
     }
+    let result=[0,0,0,0];
+    let colorplay= [0,0,0,0]
+    for(let i = 0; i<stringColorPlayer.length;i++){
+        if(colorRandom[i]==stringColorPlayer[i])
+        {
+            result[i]=2;
+            colorplay[i]=1;
+    }
 }
+    for(let i=0; i<result.length;i++)
+    {
+    let colortarget = stringColorPlayer[i];
+    for(let o=O; o<result.length;o++)
+    {
+        if(colortarget==colorRandom [o])
+        {
+            if(colorplay[i]===0)
+            {
+                result[i]=1;
+                break;
+            }
+        }
+    }
+}
+let showResult = document.querySelectorAll('.string');
+showResult.forEach(item=> {
+    if(item.classList[1]==string)
+    {
+        showResult=item.children[0];
+    }
+})
+for(let i =0;i<showResult.children.length;i++)
+{
+    if(result[i]===2)
+    {
+        showResult.children[i].children[0].className='gold';
+    }else if(result[i]===1)
+    {
+        showResult.children[1].children[0].className='silver';
+    }
+}
+win(result);
+loss(result,string);
+}
+function win(resulter)
+{
+    let win=true;
+    resulter.forEach(element => {
+        if(element!==2)
+        {
+            win=false
+        }
+    });
+    if(win)
+    {
+        bandeauResult.innerText='you win !';
+        bandeauResult.style='display:block';
+    }
+}
+function loss(resulter,string)
+{
+    let looser=false;
+    if(string==10)
+    {
+        resulter.forEach(element => {
+            if(element!==2)
+            {
+                looser=true
+            }
+        });
+        if(looser)
+        {
+            bandeauResult.innerText='you loose !';
+            bandeauResult.style='display:block';
+        }
+    }
+
+}
+let string = 1;
+let colorRandom = shufflePawns();
 let startButton = document.getElementById('play');
 startButton.addEventListener('click',playBoardStarter);
 button(true);
-
-//TO DO : fonction de vérification de la combianaison couleur/position de la ligne en cours
-// function compare () {
-//     var isMatch = true;
-//     var codeCopy = code.slice(0);
-
-// // pion de bonne couleur ET bien placé
-//     for (var i = 0; i < code.length; i++) {
-//       if (guess[i] === code[i]) {
-//         //insertPawn('hit');
-//         codeCopy[i] = 0;
-//         guess[i] = -1;
-//       } else
-//         isMatch = false;
-//     }
-
-// // pion de bonne couleur MAIS mal placé
-//     for (var j = 0; j < code.length; j++) {
-//       if (codeCopy.indexOf(guess[j]) !== -1) {
-//         //insertPawn('almost');
-//         codeCopy[codeCopy.indexOf(guess[j])] = 0;
-//       }
-//     }
-// }
+let bandeauResult = document.getElementById('result');
+bandeauResult.addEventListener('click',function()
+{
+    this.style='display:none';
+    colorRandom = shufflePawns();
+    playBoardStarter();
+});
